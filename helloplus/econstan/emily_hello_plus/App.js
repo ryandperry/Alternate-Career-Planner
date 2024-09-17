5d9dd901dd4ec02ba15252eee1fba1dc7947193e
Emily Constantin <econstan@vols.utk.edu>
2024-09-17 12:55:46 -0400

    expo practice app with image

--- helloplus/econstan/emily_hello_plus/App.js
+++ helloplus/econstan/emily_hello_plus/App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const PlaceholderImage = require('./assets/images/anne_lister.jpeg');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.Container}>
+      <View style={styles.imageContainer}>
+       <Image source={PlaceholderImage} style={styles.image} />
+        <Text style={{ color: '#9a18ac' }}>
+        Open up App.js to start working on your app!
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#pink',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 800,
+    height: 500,
+    borderRadius: 18,
   },
 });
