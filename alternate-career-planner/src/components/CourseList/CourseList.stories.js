0ba7913bd3900bbd0b3ce678172daf53f1e53c23
jckmnhn <121458470+jckmnhn@users.noreply.github.com>
2024-09-26 12:33:00 -0400

    Added storybook including a preview for the CourseList component to test in isolation. Storybooks (previews) can be viewed by running 'npm run storybook'.

--- alternate-career-planner/src/components/CourseList/CourseList.stories.js
+++ alternate-career-planner/src/components/CourseList/CourseList.stories.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import './App.css';
-import CourseListView from './components/CourseList/CourseList';
+import '../../index.css'; // Imports global css
+import CourseListView from './CourseList';
 
-function App() {
+export default {
+    title: 'CourseList',
+    component: CourseListView,
+};
 
-  // Dummy data for the course list. This will be removed later.
-  const dummyDataCourses = [
+const Template = (args) => <CourseListView {...args} />
+
+// Test data of a list of courses
+const dummyDataCourses = [
     {
       name: 'COSC 101',
       description: 'An introduction to computational thinking and structured programming.',
@@ -21,15 +26,16 @@ function App() {
       description: 'Design, analysis, and implementation of fundamental data structures and algorithms including lists, stacks, queues, hash tables, binary search trees and heaps.',
       courseHours: 4
     }
-  ];
+];
 
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <p>Below is a test for a list of courses using dummy data. Feel free to change App.js to a homepage.</p>
-      <CourseListView courses={dummyDataCourses} />
-    </div>
-  );
-}
+// A preview of a course list using dummy data
+export const Default = Template.bind({});
+Default.args = {
+    courses: dummyDataCourses,
+};
 
-export default App;
+// A preview of an empty course (nothing should be displayed)
+export const EmptyCourseList = Template.bind({});
+EmptyCourseList.args = {
+    courses: []
+};
