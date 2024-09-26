2b0193f02f4ef9f73c47550f5078bd83bb537691
jckmnhn <121458470+jckmnhn@users.noreply.github.com>
2024-09-26 15:28:15 -0400

    Added styled header image to MajorDetailView. There will be a header image unique to each major displayed in this view.

---
+++ alternate-career-planner/src/components/MajorHeaderImage/MajorHeaderImage.js
@@ -0,0 +1,31 @@
+/* MajorHeaderImage.js
+ * Image header for a major (used in MajorDetailView)
+ */
+
+import React from 'react';
+import './MajorHeaderImage.css';
+
+// Map header images to major names
+// Images from UTK major pages
+const imageMap = {
+    'Computer Science': '/assets/major-header-images/computer-science-header.jpg',
+    'Computer Engineering': '/assets/major-header-images/computer-science-header.jpg',
+    'Electrical Engineering': '/assets/major-header-images/electrical-engineering-header.jpg',
+};
+
+// Returns a header image
+const MajorHeaderImage = ({ major }) => {
+    // Return a generic header image if there is not one for that major
+    const imageURL = imageMap[major.name] || 'assets/major-header-images/generic-header-image.jpg';
+
+    return (
+        <header className="header-image">
+            <img
+             src={imageURL}
+             alt={major.name}
+            />
+        </header>
+    );
+};
+
+export default MajorHeaderImage;
