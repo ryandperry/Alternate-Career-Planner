2ea661f9ea244b5d5a7d4fa8ccfd6e455a304a88
jckmnhn <121458470+jckmnhn@users.noreply.github.com>
2024-10-02 16:45:01 -0400

    Added MajorsListView and MajorList component. MajorsListView is a page displaying a list of similar majors for the user and will likely be displayed after the initial interest quiz or file upload. MajorsListView and its component include stories.js files for testing.

---
+++ alternate-career-planner/src/views/MajorsListView/MajorsListView.js
@@ -0,0 +1,21 @@
+/* MajorsListView.js
+ * Description: Displays a list of similar majors for the user.
+ *  This page will likely be displayed after the initial interest quiz.
+ */
+
+import React from 'react';
+import './MajorsListView.css';
+import MajorList from '../../components/MajorList/MajorList';
+
+// Lists similar majors and their descriptions
+const MajorsListView = ({ majors }) => {
+    return (
+        <div>
+            <h1> Similar Majors 🎓 </h1>
+            <p> Thanks for taking the interest quiz! Below are some majors that we think you'll enjoy. </p>
+            <MajorList majors = {majors}/>
+        </div>
+    )
+}
+
+export default MajorsListView;
