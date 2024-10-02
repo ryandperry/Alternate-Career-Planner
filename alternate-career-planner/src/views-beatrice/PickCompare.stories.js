590a4aeadc896786a0a4cac6c3a811ac16671b0e
bea-eldridge <beldrid4@vols.utk.edu>
2024-10-02 17:07:25 -0400

    debugging

--- alternate-career-planner/src/views-beatrice/PickCompare.stories.js
+++ alternate-career-planner/src/views-beatrice/PickCompare.stories.js
@@ -31,14 +31,31 @@ const dummyDataCourses = [
 // Test data for a major and its description
 const dummyMajor = {
     name: 'Computer Science',
-    description: 'Computer Science is the systematic study of algorithmic processes that describe ' +
-     'and transform information: their theory, analysis, design, efficiency, implementation, ' +
-     'and application. The fundamental question underlying all of computing is: What can efficiently be automated?\n\n' +
-     'Computer Science is far more than programming. It incorporates the paradigms of theory (mathematical), ' +
-     'abstraction (experimental), and design (engineering) within many topic areas.',
+    description: `Computer Science is the systematic study of algorithmic processes that describe
+    and transform information: their theory, analysis, design, efficiency, implementation,
+    and application. The fundamental question underlying all of computing is: What can efficiently be automated?
+
+    Computer Science is far more than programming. It incorporates the paradigms of theory (mathematical),
+    abstraction (experimental), and design (engineering) within many topic areas.`,
 };
 
 const genericMajor = {
     name: 'Generic Major',
-    description: 'This is a generic major. It is a long established fact that a reader will be distracted ' +
-     'by the readable content of a page when looking at its layout. \n\nThe point of using Lorem Ipsum is that it '
+    description: `This is a generic major. It is a long-established fact that a reader will be distracted 
+    by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it 
+    has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here,' making 
+    it look like readable English.`,
+};
+
+// A preview of a major's details using dummy data
+export const ComputerScience = Template.bind({});
+ComputerScience.args = {
+    major: dummyMajor,
+    courses: dummyDataCourses,
+};
+
+export const GenericName = Template.bind({});
+GenericName.args = {
+    major: genericMajor,
+    courses: dummyDataCourses,
+};
