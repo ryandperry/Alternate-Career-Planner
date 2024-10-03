375ee7c6e4e9f10d9449512a2e133283803740c2
KaleDod <lsd728@vols.utk.edu>
2024-10-02 20:59:44 -0400

    Created Home page in App.js as well as a view

---
+++ alternate-career-planner/src/views/views-kale/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './HomeScreen.css';
+
+function HomeScreen(){
+    return (
+        <div>
+            <h1>Alternate Career Planner</h1>
+            <h2>Quick Explination</h2>
+            <p>         Welcome to the Alternate Career Planner, this will allow to upload your 
+                completed courses and we will show you the alternate majors that you can change to. 
+                You can also take a short quiz and we can give you some majors that fit the answers 
+                you have given us. </p>
+            <div className='quiz-div'>
+            <button className='quiz-button'>Take Quiz</button>
+            </div>
+            <div className='upload-div'>
+            <button className='upload-button'>Upload Completed Courses</button>
+            </div>
+        </div>
+    );
+}
+
+export default HomeScreen;
