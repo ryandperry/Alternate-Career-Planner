f5cb0ea5274663d1f96b00a0dc70ee6856243e27
Ryan Perry <rperry21@vols.utk.edu>
2024-10-03 02:22:08 -0400

    #5 fixed indentation of ProgressBar files

--- alternate-career-planner/src/components/ProgressBar/ProgressBar.js
+++ alternate-career-planner/src/components/ProgressBar/ProgressBar.js
@@ -6,36 +6,36 @@
 import React, { useState } from "react";
 
 export const ProgressBar = () => {
-  const [progress, setProgress] = useState(0);
-  // Increments progress by 20% on button press
-  const handleButtonClick = ()=>{
-    if (progress < 100) {
-      setProgress(progress + 20);
+    const [progress, setProgress] = useState(0);
+    // Increments progress by 20% on button press
+    const handleButtonClick = ()=>{
+        if (progress < 100) {
+            setProgress(progress + 20);
+        }
     }
-  }
 
-  // Resets progress on button press
-  const handleButtonReset = () => {
-    setProgress(0);
-  }
+    // Resets progress on button press
+    const handleButtonReset = () => {
+        setProgress(0);
+    }
 
-  const getColor = () => {
-    return "#ff8200";
-  }
+    const getColor = () => {
+        return "#ff8200";
+    }
 
-  return <div className="container">
-    <div className="progress-bar">
-      <div 
-        className="progress-bar-fill" 
-        // Width of progress bar increases as progress increases
-        style={{ width: `${progress}%`,
-                 backgroundColor: getColor() }}
-      >
-        {" "}
-      </div>
-    </div>
-    <div className="progress-label">{progress}%</div>
-    <button onClick={handleButtonClick}>Progress</button>
-    <button onClick={handleButtonReset}>Reset</button>
-  </div>;
+    return <div className="container">
+        <div className="progress-bar">
+            <div 
+                className="progress-bar-fill" 
+                // Width of progress bar increases as progress increases
+                style={{ width: `${progress}%`,
+                                 backgroundColor: getColor() }}
+            >
+                {" "}
+            </div>
+        </div>
+        <div className="progress-label">{progress}%</div>
+        <button onClick={handleButtonClick}>Progress</button>
+        <button onClick={handleButtonReset}>Reset</button>
+    </div>;
 };
