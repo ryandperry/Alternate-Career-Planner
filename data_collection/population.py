3441bdc330d7b7e824a71a625b1c1e57ea93ab23
bea-eldridge <beldrid4@vols.utk.edu>
2024-10-15 08:51:49 -0400

    add population.py

---
+++ data_collection/population.py
@@ -0,0 +1,22 @@
+# Pandas Data Frames 
+
+# Bucket Table To-Dos: 
+# Populate the course ID's, fill in the number of courses 
+
+# for i in bucket ID 
+
+# Prereq/Coreq 
+# Populating the Course ID and the Prereq Bucket Name and number of courses/ hours 
+
+
+# for i in Major Table 
+# Course ID to Required Course & Required Course Name 
+# Bucket ID to course name and number of hours 
+
+# Class :
+# course : ID, name description 
+# Bucket: ID, Bucket Name, Bucket NUmber of Hours/Number of COurses, List of Courses
+# 
+# Major: ID, Abbreviation, Name, Description  
+
+# Write out the classes , then write out stump code 
