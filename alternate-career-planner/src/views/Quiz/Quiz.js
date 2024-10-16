9e9e32e9cb942b17f8b95ed1b3cf7cf3c4a5b08c
Ryan Perry <rperry21@vols.utk.edu>
2024-10-16 19:37:04 -0400

    #19 basic quiz format

--- alternate-career-planner/src/views/Quiz/Quiz.js
+++ alternate-career-planner/src/views/Quiz/Quiz.js
@@ -1,10 +1,24 @@
+/* Quiz.js
+ * Description: Diplays the major interest quiz.
+ */
+
 import React from "react";
+
+import './Quiz.css';
+import QuizComp from './../../components/QuizComp/QuizComp';
+
 const Quiz = () => {
     return (
-        <div>
-            <h1>Quiz</h1>
-            <div>Have some content</div>
+        <div className='Quiz'>
+            <div className='text-section'>
+                <h1>Interest Quiz</h1>
+                <p> Answer a few questions to find out 
+                    which major is right for you. </p>
+            </div>
+            
+            <QuizComp />
         </div>
     );
 };
+
 export default Quiz;
