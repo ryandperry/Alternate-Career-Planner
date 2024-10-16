9481a210d9273f6ed1c61cd5cd811b09a9a75e5d
Ryan Perry <rperry21@vols.utk.edu>
2024-10-16 19:35:31 -0400

    modified home screen

--- alternate-career-planner/src/views/Home/Home.js
+++ alternate-career-planner/src/views/Home/Home.js
@@ -6,11 +6,12 @@ const Home = () => {
         <div className='Home'>
             <div className='text-section'>
                 <h1>We make switching majors easy.</h1>
-                <p> Welcome to the Alternate Career Planner! Like engineering
-                    but hate your major? Don't worry, you've come to the 
-                    right place. Upload your academic history file and find 
-                    majors that you could easily switch to, or take a short
-                    quiz to find out which major is right for you. </p>
+                <p> Welcome to the Alternate Career Planner! Like 
+                    engineering but hate your major? Don't worry, you've
+                    come to the right place. Upload your academic 
+                    history file and find majors that you could easily 
+                    switch to, or take a short quiz to find out which 
+                    major is right for you. </p>
                 <div className='buttons'>
                     <button className='upload-button'>Upload File</button>
                     <Link to='/quiz'>
