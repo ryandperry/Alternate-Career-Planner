0958318bd8273985e9bc692350a28b92f953999b
Ryan Perry <rperry21@vols.utk.edu>
2024-10-16 13:25:17 -0400

    #19 quiz screen template

---
+++ alternate-career-planner/src/views/Quiz/Quiz.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Quiz = () => {
+    return (
+        <div>
+            <h1>Quiz</h1>
+            <div>Have some content</div>
+        </div>
+    );
+};
+
+export default Quiz;
