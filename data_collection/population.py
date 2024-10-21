421a3503568ad0a64b3fe60d90bf64de1a7b0057
Emily Constantin <econstan@vols.utk.edu>
2024-10-21 18:05:08 -0400

    notes and changes from meeting

--- data_collection/population.py
+++ data_collection/population.py
@@ -46,6 +46,9 @@ class Course:
     self.name = name
     self.description = description
     self.hours = hours
+    
+  prereq_courseids = {1, 4, 6, 7}
+  coreq_courseids = set()
 
 
 class Bucket:
@@ -55,24 +58,38 @@ class Bucket:
     self.course_names = course_names
     self.num_hours = num_hours
     self.num_courses = num_courses
+  
+  course_ids = set()
+  bucket_ids = set()
 
 #script to read in course table, major table, and bucket table
 # print("Hello World")
-try:
-  df = pd.read_csv('database_design_CT.csv', dtype=str)
-except:
-  df = pd.read_csv('data_collection/database_design_CT.csv', dtype=str)
-#print(df.shape[0])
-course_objects = {}
-for row in df.index:
-    #course_id, name, description, hours
-    course_id = df.loc[row, "Course ID"]
-    course_object = Course(df.loc[row, 'Course ID'], df.loc[row, 'Course'], df.loc[row, 'Course Description'], df.loc[row, 'Course Hours'])
-    #dict[i] = course_object
-    course_objects[str(course_id)] = course_object
-    # print(dict[str(row)].name)
-    # print(course_object.name)
-print(course_objects)
+def build_course_objects():
+  try:
+    df = pd.read_csv('database_design_CT.csv', dtype=str)
+  except:
+    df = pd.read_csv('data_collection/database_design_CT.csv', dtype=str)
+  #print(df.shape[0])
+  course_objects = {}
+  for row in df.index:
+      #course_id, name, description, hours
+      course_id = df.loc[row, "Course ID"]
+      course_object = Course(df.loc[row, 'Course ID'], df.loc[row, 'Course'], df.loc[row, 'Course Description'], df.loc[row, 'Course Hours'])
+      #dict[i] = course_object
+      course_objects[str(course_id)] = course_object
+      # print(dict[str(row)].name)
+      # print(course_object.name)
+  print(course_objects)
+
+# PREREQ and COREQ
+#use the csv for prereq/coreq table
+#for each row (each course and P/C pairing)
+#find the course ID in the list of course objects
+  # have to look through the course_objects dictionary
+  # for each course in course_objects.values():
+    # if course.name == prereq_table_course.name
+    # depending on the P/C flag
+    # add the "req" to the set for this course
 
 #looping through major table rows
 #build major class object for each row
