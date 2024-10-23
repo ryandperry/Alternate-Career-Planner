036499a9d8685b14dc3c090246b903f2b6fa47fa
bea-eldridge <beldrid4@vols.utk.edu>
2024-10-23 10:15:45 -0400

    merging the regular with mine, kind of

--- data_collection/population_be.py
+++ data_collection/population_be.py
@@ -28,6 +28,14 @@
 
 import pandas as pd
 
+filenames = {
+  "Bucket Table": "bucket_table.csv",
+  "Major Table": "database_design_MT.csv",
+  "Course Table": "database_design_CT.csv",
+  "PreReq/CoReq Table": "prereq_coreq.csv",
+  "Major Requirements Folder Name": "major_requirement_csvs",
+  "Major Requirements Prefix": "major_"
+}
 
 class Major:
   def __init__(self, major_id, abbr, name, description):
@@ -139,6 +147,35 @@ def build_bucket_object():
       # print(major_obj.name)
   print(bucket_objects)
 
+def build_bucket_objects(course_objects):
+  try:
+    bucket_table = pd.read_csv(filenames['Bucket Table'], dtype=str)
+  except:
+    bucket_table = pd.read_csv('data_collection/'+filenames['Bucket Table'], dtype=str)
+  bucket_table= bucket_table.fillna("NULL")
+  bucket_objects = {}
+  for row in bucket_table.index:
+      bucket_id = bucket_table.loc[row, "Bucket ID"]
+      bucket_obj = Bucket(bucket_table.loc[row, 'Bucket ID'], bucket_table.loc[row, 'Bucket Name'], bucket_table.loc[row, 'Bucket Number of Hours'], bucket_table.loc[row, "Bucket Number of Courses"])
+
+      #seperate out the course names in semi-colon seperated list
+      bucket_obj.course_names = set(bucket_table['Course Names'][row].replace(" ", "").split(";"))
+
+      # #TODO this doesnt work. need to add course IDs
+      # for current_course_name in bucket_obj.course_names:
+      #   # find this course ID
+      #   for course_obj in course_objects.values():
+      #     if(course_obj.name == current_course_name):
+      #       bucket_obj = bucket_obj.course_ids.add(course_obj.course_id)
+
+      #add other bucket IDs
+      bucket_obj.bucket_ids.add(bucket_table['Sequence Bucket IDs'][row])
+      # save this new bucket object
+      bucket_objects[str(bucket_id)] =  bucket_obj
+
+
+  return bucket_objects
+
 
 
 #script to match all major's requirements to the associated
@@ -153,59 +190,59 @@ def build_bucket_object():
 # right now we only have 4 accessbile csvs but in the future, we 
 # will have access to all 12
 # for major_object_key in major_objects.keys():
-for major_object_key in ["1", "5","10", "11"]:
+# for major_object_key in ["1", "5","10", "11"]:
 
-  #read csv for this major using the ID number
-  major_object = major_objects[major_object_key]
-  print(major_object.name)
-  # print(major_object.bucket_ids)
+#   #read csv for this major using the ID number
+#   major_object = major_objects[major_object_key]
+#   print(major_object.name)
+#   # print(major_object.bucket_ids)
   
-  relative_filename = 'major_requirement_csvs/major_' + str(int(major_object.major_id)) + '.csv'
-  outside_filename = 'data_collection/major_requirement_csvs/major_' + str(int(major_object.major_id)) + '.csv'
-  print(relative_filename)
-  try:
-    major_requirements = pd.read_csv(relative_filename, dtype=str)
-  except:
-    major_requirements = pd.read_csv(outside_filename, dtype=str)
-  #fill in everything thats null in the excel 
-  major_requirements = major_requirements.fillna("NULL")
+#   relative_filename = 'major_requirement_csvs/major_' + str(int(major_object.major_id)) + '.csv'
+#   outside_filename = 'data_collection/major_requirement_csvs/major_' + str(int(major_object.major_id)) + '.csv'
+#   print(relative_filename)
+#   try:
+#     major_requirements = pd.read_csv(relative_filename, dtype=str)
+#   except:
+#     major_requirements = pd.read_csv(outside_filename, dtype=str)
+#   #fill in everything thats null in the excel 
+#   major_requirements = major_requirements.fillna("NULL")
 
-  #make the course and bucket objects using either the ID or the name
-  #example: if the row has a course (MATH 141), lookup in the dictionary
-    #of courses for that and add the course ID to major.course_ids set()
-  #example: if the row has course ID 5, then add that ID
-  for row in major_requirements.index:
-    # print(row)
-    #get the course ID or bucket ID for this major requirement
-    bucket_ID = major_requirements['Required Bucket ID'][row]
-    course_ID = major_requirements['Required Course ID'][row]
-    course = major_requirements['Required Course'][row]
+#   #make the course and bucket objects using either the ID or the name
+#   #example: if the row has a course (MATH 141), lookup in the dictionary
+#     #of courses for that and add the course ID to major.course_ids set()
+#   #example: if the row has course ID 5, then add that ID
+#   for row in major_requirements.index:
+#     # print(row)
+#     #get the course ID or bucket ID for this major requirement
+#     bucket_ID = major_requirements['Required Bucket ID'][row]
+#     course_ID = major_requirements['Required Course ID'][row]
+#     course = major_requirements['Required Course'][row]
   
-    #if this row has a bucket, use the bucket ID
-    if(bucket_ID != "NULL"):
-      # print("bucket ID is ", bucket_ID)
-      major_object.bucket_ids.add(str(bucket_ID))
-      # print("bucket ids list is ", major_object.bucket_ids)
+#     #if this row has a bucket, use the bucket ID
+#     if(bucket_ID != "NULL"):
+#       # print("bucket ID is ", bucket_ID)
+#       major_object.bucket_ids.add(str(bucket_ID))
+#       # print("bucket ids list is ", major_object.bucket_ids)
     
-    elif(course_ID != "NULL"):
-      # print("course ID is ", course_ID)
-      major_object.course_ids.add(str(course_ID))
-      # print("course ids list is ", major_object.course_ids)
+#     elif(course_ID != "NULL"):
+#       # print("course ID is ", course_ID)
+#       major_object.course_ids.add(str(course_ID))
+#       # print("course ids list is ", major_object.course_ids)
     
-    elif(course != "NULL"):
-      #get all values for the course objects
-      #search in the values for the course.name that matches
-      #get course ID
-      #add to the course_ids for this major
-      course_ID = [course_obj.course_id for course_obj in course_objects.values() if course_obj.name == course]
-      # print("found a course id from the name")
-      major_object.course_ids.add(str(course_ID))
+#     elif(course != "NULL"):
+#       #get all values for the course objects
+#       #search in the values for the course.name that matches
+#       #get course ID
+#       #add to the course_ids for this major
+#       course_ID = [course_obj.course_id for course_obj in course_objects.values() if course_obj.name == course]
+#       # print("found a course id from the name")
+#       major_object.course_ids.add(str(course_ID))
     
-    # else:
-      #this isnt a valid requirement because theres nothing in this row
-      # print("empty row: major requirement has neither course nor bucket listed")
-  print(major_object.bucket_ids)
-  print(major_object.course_ids)
+#     # else:
+#       #this isnt a valid requirement because theres nothing in this row
+#       # print("empty row: major requirement has neither course nor bucket listed")
+#   print(major_object.bucket_ids)
+#   print(major_object.course_ids)
 
 # emily to do
 # test the civil engineering excel
@@ -227,7 +264,20 @@ def compare_academic_history(person_object, major_objects, course_objects):
       continue
     else:
       for course_taken in person_object.classes_array:
-        #somewhere in here we need to check if it is part of a bucket 
+        #somewhere in here we need to check if it is part of a bucket
         if(major_objects[i].course_id == course_taken[i]):
           hour_counter += course_objects["course_taken"].hours
         # look up the course id in the major dictionary at this i 
+
+def main():
+  course_objects = build_course_objects()
+  bucket_objects = build_bucket_objects(course_objects)
+  print(course_objects)
+  print(bucket_objects)
+  majorid = 1
+  classes_array = ["EF 151", "EF 230", "MATH 141"]
+  quiz_results = "ME"
+  person_object = Person(major=majorid, classes_array=classes_array, quiz_results=quiz_results)
+  major_objects = build_major_objects(course_objects=course_objects, bucket_objects=bucket_objects)
+  print(major_objects)
+  compare_academic_history(person_object=person_object, major_objects=major_objects, course_objects=course_objects)
