3405dfa397dba85be8fef7e89e0a63f8029eeff9
jckmnhn <121458470+jckmnhn@users.noreply.github.com>
2024-10-23 15:37:35 -0400

    Added all majors and their descriptions from the UTK catalog for testing #25

--- alternate-career-planner/src/views/ResultsView/ResultsView.js
+++ alternate-career-planner/src/views/ResultsView/ResultsView.js
@@ -12,30 +12,55 @@ import MajorsListView from '../MajorsListView/MajorsListView'
 // Lists the details of a given major including a description and required courses
 const ResultsView = ({ majors }) => {
 
-    // Majors for testing until they can be passed from a datasheet
+    // Majors and their descriptions all taken from UTK catalog
     const testMajors = [
         {
-        name: 'Computer Science',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut \
-        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \
-        aliquip ex ea commodo consequat.',
+        name: 'Aerospace Engineering',
+        description: 'Aerospace engineering uses the basic sciences and mathematics to develop the foundation for the design, development, production, testing, and applied research associated with aerospace vehicles. These vehicles include aircraft, spacecraft, and missiles. Auxiliary and propulsion systems are also an integral part of this education. These include guidance, control, environmental, ramjet, rocket, turbo-jet, and piston engine systems. Emphasis in the senior year is directed toward these topics, and the program culminates in a major aerospace design project.',
         },
         {
-        name: 'Mechanical Engineering',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut \
-        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit \
-        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        name: 'Biomedical Engineering',
+        description: 'The biomedical engineering (BME) curriculum integrates selected engineering sciences and design methods with life science coursework. The program prepares students for careers in a variety of health care related professions including work for medical device manufacturers and regulatory governmental agencies. The course content of the biomedical engineering curriculum complements the departmental strengths in mechanical engineering and includes a comprehensive coverage of engineering materials and biomechanics applications. Elective courses are available to allow students to specialize their curriculum to areas of particular current interest in the marketplace such as cellular and tissue engineering applications. The biomedical engineering program also allows students to meet medical school admission requirements with an appropriate selection of technical electives.',
+        },
+        {
+        name: 'Chemical Engineering',
+        description: 'The undergraduate chemical engineering program provides a comprehensive education through broad-based course offerings in fundamental engineering science and in specialized courses that provide depth of knowledge in areas of particular interest.',
+        },
+        {
+        name: 'Civil Engineering',
+        description: 'The undergraduate civil engineering program provides a broad-based civil and environmental engineering education that builds off the Tickle College of Engineering’s engage Engineering Fundamentals Program. Courses in each of the major areas of civil and environmental engineering are offered at the junior and senior level. Through technical electives, students can gain a greater depth of knowledge in specializations of interest. Students desiring an even greater depth of knowledge in an area will typically pursue a master’s degree.',
         },
         {
         name: 'Computer Engineering',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\nUt \
-        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit \
-        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        description: 'Computer engineering deals with the design, construction, and implementation of the hardware and software components that comprise computers, computer-controlled systems, and networks of computing devices. Modern computing appears everywhere in our daily lives, including the development and maintenance of systems such as many-core computer processors, internet-of-things (IoT) systems, and cyberphysical control systems, to name a few. Example applications include smart phones, autonomous vehicles, robotics, automotive embedded systems, voice-controllable systems, image processing, and supercomputer architectures.\n\nTypically, a computer engineering curriculum provides a background in three broad areas—hardware, software, and hardware-software integration. Students will also have the opportunity to explore fundamental topics such as microprocessors, computer architecture, digital signal processing, embedded systems, operating systems, network architectures, data communications, cybersecurity, and other related material. In addition, the program includes core engineering subjects that are common to all engineering disciplines.',
+        },
+        {
+        name: 'Computer Science',
+        description: 'Computer Science is the systematic study of algorithmic processes that describe and transform information: their theory, analysis, design, efficiency, implementation, and application. The fundamental question underlying all of computing is: What can efficiently be automated?\n\nComputer Science is far more than programming. It incorporates the paradigms of theory (mathematical), abstraction (experimental), and design (engineering) within many topic areas.',
+        },
+        {
+        name: 'Electrical Engineering',
+        description: 'Electrical engineering deals with the application of the physical laws governing charged particles. From miniature integrated circuits that contain millions of microelectronic devices, to high-speed fiber-optic communication systems that span international boundaries, electrical engineering impacts every aspect of modern-day living.\n\nElectrical engineering is unique among the engineering disciplines because of its wide range of applications. Subject areas are so diverse, it is not always apparent that there is an underlying connection. The range of subjects is not only broad but also expanding.',
+        },
+        {
+        name: 'Environmental Engineering',
+        description: 'Environmental engineering offers students the opportunity to explore multidisciplinary solutions to solve problems around protecting human health and the environment from the effects of pollution and creating a sustainable future. They tackle problems related to sustainability, managing water supplies, air pollution, and industrial waste.',
+        },
+        {
+        name: 'Industrial Engineering',
+        description: 'Our undergraduate curriculum is designed to prepare our graduates to achieve the following program objectives 3-5 years after graduation:\n\nObjective 1: Have successful professional careers based on industrial engineering knowledge and skills.\n\nObjective 2: Engage in professional development practices that enable them to remain competent in the ever expanding discipline of industrial engineering.\n\nObjective 3: Provide strong leadership and quality service to their organization, community, and society.',
+        },
+        {
+        name: 'Materials Science & Engineering',
+        description: 'At the Department of Materials Science Engineering, we stay on the leading edge of technology through the development of new materials and the improvement of existing materials for applications in all engineering fields. As an interdisciplinary field, MSE is at the forefront of modern technological advances, offering students a stepping stone to solving tomorrow’s grand challenges as they take their next steps toward their future in engineering.',
+        },
+        {
+        name: 'Mechanical Engineering',
+        description: 'Mechanical engineering involves the design, analysis, testing, and manufacture of mechanical and thermal systems. Mechanical engineers are employed in nearly every industry, from basic research through mass production of energy systems, computer software/hardware, robotics, and vehicles of all kinds. Due to high demand for mechanical engineers and the excellent undergraduate program at UT, the mechanical engineering degree is one of the largest degree programs.',
+        },
+        {
+        name: 'Nuclear Engineering',
+        description: 'The most visible example of nuclear engineering is the production of nuclear energy, which is sustainable, safe, and has no carbon footprint. But nuclear engineering is so much more than that. Nuclear engineers also: help the medical field diagnose and treat diseases and sterilize equipment, identify and analyze materials such as pollutants, provide an added level of security to transportation, aid in the diagnostics and maintenance of structures and materials, and provide a more reliable, cost-effective power supply for use in space exploration.'
         }
     ];
 
