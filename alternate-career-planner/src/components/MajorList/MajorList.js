02a531f446f54b5adba3b5241a79d501db1a4240
jckmnhn <121458470+jckmnhn@users.noreply.github.com>
2024-10-24 09:03:20 -0400

    List of majors now properly link to each major on results page #25

--- alternate-career-planner/src/components/MajorList/MajorList.js
+++ alternate-career-planner/src/components/MajorList/MajorList.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './MajorList.css'
+import './MajorList.css';
+import {Link} from 'react-router-dom';
 
 // Lists majors and their descriptions 
 const MajorList = ({ majors }) => {
@@ -8,8 +9,10 @@ const MajorList = ({ majors }) => {
             <ul>
                 {majors.map((major, index) => (
                     <li key={index} className="majorlist-item">
-                        <h3 className="majorlist-title">{major.name}</h3>
-                        <p className="majorlist-description">{major.description}</p>
+                        <Link to={`/results/majors/${encodeURIComponent(major.name)}`}>
+                            <h3 className="majorlist-title">{major.name}</h3>
+                            <p className="majorlist-description">{major.description}</p>
+                        </Link>
                     </li>
                 ))}
             </ul>
