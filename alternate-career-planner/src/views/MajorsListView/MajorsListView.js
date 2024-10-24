499c12e238cd35755e99671304fac68f7be47a68
Ryan Perry <rperry21@vols.utk.edu>
2024-10-24 16:51:22 -0400

    #29 additional formatting

--- alternate-career-planner/src/views/MajorsListView/MajorsListView.js
+++ alternate-career-planner/src/views/MajorsListView/MajorsListView.js
@@ -10,7 +10,6 @@ import { useLocation, Link } from 'react-router-dom'
 
 // Lists similar majors and their descriptions
 const MajorsListView = ({ majors }) => {
-    // useLocation helps display quiz results from quiz component
     const location = useLocation();
     const finalMajor = location.state?.finalMajor;
 
@@ -18,12 +17,11 @@ const MajorsListView = ({ majors }) => {
         <div>
             <h1 className='similarMajorsHeader'> Quiz Results 🎓 </h1>
             <p> Thanks for taking the quiz! Based on your provided answers, we think you'll enjoy: </p>
-          <p className="majorlist-item">
-              <Link to={`/results/majors/${encodeURIComponent(finalMajor)}`}>
-                  <h3 className="majorlist-title">{finalMajor}</h3>
-              </Link>
-          </p>
-
+            <p className="majorlist-item">
+                <Link to={`/results/majors/${encodeURIComponent(finalMajor)}`}>
+                    <h3 className="majorlist-title">{finalMajor}</h3>
+                </Link>
+            </p>
             <p>Alternatively, explore all majors below. </p>
             <MajorList majors = {majors}/>
         </div>
