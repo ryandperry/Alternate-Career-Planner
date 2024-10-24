fae16a396d166df625529d720109eb715ee5774d
Ryan Perry <rperry21@vols.utk.edu>
2024-10-24 15:49:26 -0400

    #29 Quiz Component follows different paths depending on the user's answers

--- alternate-career-planner/src/components/QuizComp/QuizComp.js
+++ alternate-career-planner/src/components/QuizComp/QuizComp.js
@@ -1,165 +1,140 @@
 /* QuizComp.js
  * Description: Implements Quiz Component used on the quiz screen.
- * Users answer questions from quiszdata.js. Their answers are displayed
- * after finishing the quiz.
+ * Users answer questions from quizdata.js. Redirects to results
+ * screen on completion.
  */
 
 import React, { Component } from 'react'
-import { QuizData } from './quizdata.js'
+import { QuizData } from './quizdata2.js'
 import { Link, Navigate } from 'react-router-dom'
+
+const majorMapping = {
+    I: "Computer Science",
+    M: "Computer Engineering",
+    N: "Electrical Engineering",
+    Q: "Aerospace Engineering",
+    J: "Biomedical Engineering",
+    K: "Biosystems Engineering",
+    S: "Chemical Engineering",
+    O: "Industrial Engineering",
+    R: "Mechanical Engineering",
+    L: "Nuclear Engineering",
+    P: "Civil Engineering",
+    T: "Materials Science and Engineering"
+}
 
 export class QuizComp extends Component {
   
     state = {
         currentIndex: 0,
+        currentPath: '*',
         options: [],
-        score: 0,
+        userAnswer: null,
         disabled: true,
         quizEnd: false,
-        userAnswer: null,
-        userAnswers: []
-    }
+        finalMajor: null
+    };
 
+    /*
+     *  Loads the current question and its options 
+     *  into the state based on the current path.
+     */
     loadQuiz = () => {
-        const {currentIndex} = this.state
-        this.setState(() => {
-            return {
-                question: QuizData[currentIndex].question,
-                options: QuizData[currentIndex].options,
-            }
-        })
-    }
+        const { currentPath } = this.state;
+        const currentQuestion = QuizData.find(q => q.path === currentPath);
 
+        if (currentQuestion) {
+            this.setState({
+                question: currentQuestion.question,
+                options: currentQuestion.options,
+                currentIndex: currentQuestion.id,
+                disabled: true,
+                userAnswer: null
+            });
+        }
+    };
+
+    /*
+     * Handles the user selecting an answer.
+     * Updates the userAnswer in state and enables the "Next" button.
+     */
     checkAnswer = (selectedAnswer) => {
+        const { currentPath } = this.state;
+        const currentQuestion = QuizData.find(q => q.path === currentPath);
+
+        const selectedAnswerIndex =
+            currentQuestion.options.indexOf(selectedAnswer);
+
+        const newPath = currentQuestion.newPath[selectedAnswerIndex];
+
         this.setState({
             userAnswer: selectedAnswer,
-            disabled: false
-        })
-    }
+            disabled: false,
+            nextPath: newPath
+        });
+    };
 
+    /*
+     * Moves to the next question.
+     * Ends the quiz if the next path is a major.
+     */
     nextQuestionHandler = () => {
-        const {userAnswer, score, userAnswers, currentIndex} 
-            = this.state;
-        userAnswers[currentIndex] = userAnswer
-        this.setState ({
-            currentIndex: this.state.currentIndex + 1,
-            userAnswer: null,
-            disabled: true,
-            userAnswers: userAnswers
-        })
-    }
+        const { nextPath } = this.state;
 
-    prevQuestionHandler = () => {
-        this.setState ({
-            currentIndex: this.state.currentIndex - 1,
-            userAnswer: null,
-            disabled: true 
-        }) 
-    }
+        if (majorMapping[nextPath]) {
+            // If the path corresponds to a final major
+            this.setState({
+                quizEnd: true,
+                finalMajor: majorMapping[nextPath]
+            });
+        } else {
+            // Otherwise, update the path and load the next question
+            this.setState({ currentPath: nextPath }, this.loadQuiz);
+        }
+    };
 
-    finishHandler = () => {
-        const {userAnswer, score, userAnswers, currentIndex} 
-            = this.state;
-        userAnswers[currentIndex] = userAnswer
-
-        this.setState({
-            quizEnd: true,
-            userAnswers: userAnswers    
-        })
-    }
-
+    /*
+     * Loads the first quiz question.
+     * Runs after the component mounts.
+     */
     componentDidMount() {
         this.loadQuiz();
-    }
+    };
 
-    componentDidUpdate(prevProps, prevState) {
-        const {currentIndex} = this.state
-
-        if (this.state.currentIndex !== prevState.currentIndex) {
-            this.setState(() => {
-                return {
-                    question: QuizData[currentIndex].question,
-                    options: QuizData[currentIndex].options,
-                }
-            })
-           
-        }
-    }
-
+    /*
+     * Renders the quiz intereface.
+     * Displays the final major if the quiz ends.
+     */
     render() {
-          const {question, options, currentIndex, userAnswer, quizEnd,
-                 userAnswers} = this.state
-          
-          if (quizEnd) {
-              return <Navigate to= "/results" />;
+        const { question, options, quizEnd, 
+                userAnswer, disabled, finalMajor} = this.state;
 
+        if (quizEnd) {
+            return <Navigate to="/results" state={{ finalMajor }} />;
+        }
 
-                  /*<div>
-                      <h2>You finished the quiz!</h2>
-                      <p>Here's how you answered:</p>
+        return (
+            <div>
+                <h4>{`Question ${this.state.currentIndex + 1}`}</h4>
+                <h2>{question}</h2>
 
-                      <ul>
-                          {QuizData.map((item, index) => (
-                              <li key={index}>
-                                  <strong>Q: {item.question}</strong>
-                                  <br />
-                                  {userAnswers[index]}
-                              </li>
-                          ))}
-                      </ul>
-                      
-                      <div className='buttons'>
-                          <Link to='/'>
-                              <button className='upload-button'>
-                                  Return Home
-                              </button>
-                          </Link>
+                {options.map((option, index) => (
+                    <p
+                        key={index}
+                        className={`options ${userAnswer === option 
+                                                ? 'selected' : null}`}
+                        onClick={() => this.checkAnswer(option)}
+                    >
+                        {option}
+                    </p>
+                ))}
 
-                          <button className='quiz-button'
-                            onClick={() => window.location.reload()}>
-                              Retake Quiz
-                          </button>
-                      </div>
-                  </div>*/
-          }
-
-          return (
-              <div>
-                  <h2>{question}</h2>
-                  <h4>{`Question ${currentIndex+1} of 
-                       ${QuizData.length}`}</h4>
-                  
-                  {
-                      options !== null? options.map(option =>
-                          <p key={option.id} className={`options 
-                          ${userAnswer == option?  "selected": null}`}
-                          onClick={() => this.checkAnswer(option)}>
-                              {option}   
-                          </p>
-                      ): <span>...</span>
-                  }
-                  {currentIndex < QuizData.length && 
-                      currentIndex > 0 &&
-                      <button
-                      onClick={this.prevQuestionHandler}>
-                          Previous Question
-                      </button>
-                  }
-                  {currentIndex < QuizData.length - 1 &&
-                      <button disabled = {this.state.disabled}
-                      onClick={this.nextQuestionHandler}>
-                          Next Question
-                      </button>
-                  }
-                  {currentIndex === QuizData.length - 1 &&
-                      <button disabled = {this.state.disabled}
-                      onClick={this.finishHandler}>
-                          Finish Quiz
-                      </button>
-                  }
-
-              </div>
-          )
+                <button disabled=
+                    {disabled} onClick={this.nextQuestionHandler}>
+                    Next Question
+                </button>
+            </div>
+        );
     }
 }
 
