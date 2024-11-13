22ee75029fb32951f751a5f4336231de2e7fc20e
Ryan Perry <rperry21@vols.utk.edu>
2024-11-13 18:13:31 -0500

    #45 fixed TopMajors.js formatting

--- alternate-career-planner/src/components/TopMajors/TopMajors.js
+++ alternate-career-planner/src/components/TopMajors/TopMajors.js
@@ -7,25 +7,23 @@ import {testMajors} from '../../views/ResultsView/testMajors';
 
 const TopMajors = ({ majors }) => {
     return (
-      <div>
-      {majors.map((major, index) => {
-          const majorInfo = testMajors.find(m => m.name === major);
-          const desc = majorInfo ? majorInfo.description : 
-                          "Description not available.";
-
-
-
-      return(
-          <div key={index}>
-              <hr class="solid"></hr>
-              <h1 className="major-title">#{index+1} {major}</h1>
-              <MajorHeaderImage major={{name: major || "Unknown Major"}}/>
-              <h2> Description </h2>
-              <p className="major-description">{desc}</p>
-              <JobsList majorname={major}/>
-          </div>
-      );
-      })}
+        <div>
+            {majors.map((major, index) => {
+                const majorInfo = testMajors.find(m => m.name === major);
+                const desc = majorInfo ? majorInfo.description : 
+                    "Description not available.";
+                return(
+                    <div key={index}>
+                        <hr class="solid"></hr>
+                        <h1 className="major-title">#{index+1} {major}</h1>
+                        <MajorHeaderImage major={{name: major || 
+                            "Unknown Major"}}/>
+                        <h2> Description </h2>
+                        <p className="major-description">{desc}</p>
+                        <JobsList majorname={major}/>
+                    </div>
+                );
+            })}
       </div>
     );
 }
