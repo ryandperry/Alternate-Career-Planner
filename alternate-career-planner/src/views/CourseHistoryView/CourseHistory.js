b19878c3a618d46fb3b150d55284719a815b5975
Ryan Perry <rperry21@vols.utk.edu>
2024-11-13 16:05:35 -0500

    #42 added course side bar to course_history screen

--- alternate-career-planner/src/views/CourseHistoryView/CourseHistory.js
+++ alternate-career-planner/src/views/CourseHistoryView/CourseHistory.js
@@ -1,9 +1,14 @@
+/* CourseHistory.js
+ * Description: Displays the user's course history.
+ */
+
 import React, {useEffect, useState } from "react";
 import './CourseHistory.css';
 import CourseHistory from '../../components/CourseHistory/CourseHistory';
 
 const CourseHistoryView = () => {
     const [courses, setCourses] = useState([]);
+    const [selectedFilter, setSelectedFilter] = useState("All");
 
     useEffect(() => {
         const storedCourses = localStorage.getItem('coursesTaken');
@@ -12,18 +17,57 @@ const CourseHistoryView = () => {
         }
     }, []);
 
+    const handleFilterChange = (filter) => {
+        setSelectedFilter(filter);
+    };
+
+    const filteredCourses = courses.filter(course => {
+        if (selectedFilter === "Active") {
+          return course.grade === "IP";
+        } else if (selectedFilter === "Completed") {
+          return course.grade !== "IP";
+        }
+        return true;
+    });
+
     return (
-        <div>
-            <h1 className="Header"> List of completed courses </h1>
-            <table className="WholeTable">
-                <tr>
-                    <th className="CourseHeader">Course Name</th>
-                    <th className="GradeHeader">Course Grade</th>
-                </tr>
-                <tr>
-                    <th colSpan={2}><CourseHistory courses = {courses}/></th>
-                </tr>
-            </table>
+        <div className="Split">
+            <div className="Sidebar">
+                <h1 className="Title">Courses</h1>
+                <ul>
+                    <li className="Header"
+                        className={`Header ${selectedFilter === "All" ? 
+                                                "active" : ""}`}
+                        onClick={() => handleFilterChange("All")}
+                    >
+                        All 
+                    </li>
+                    <li className="Header"
+                        className={`Header ${selectedFilter === "Active" ? 
+                                                "active" : ""}`}
+                        onClick={() => handleFilterChange("Active")}
+                    >
+                        Active 
+                    </li>
+                    <li className="Header"
+                        className={`Header ${selectedFilter === "Completed" 
+                                                ? "active" : ""}`}
+                        onClick={() => handleFilterChange("Completed")}
+                    >
+                        Completed 
+                    </li>
+                </ul>
+                <table>
+                    <th colSpan={2}>
+                        <CourseHistory courses = {filteredCourses}/>
+                    </th>
+                </table>
+            </div>
+
+            <div className="mainContent">
+                <h1>MAIN CONTENT</h1>
+            </div>
+
         </div>
     )
 }
