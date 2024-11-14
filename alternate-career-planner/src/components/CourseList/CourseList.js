5d4f21472791dd1409f5c887461e5dc8c5ea6efe
jckmnhn <121458470+jckmnhn@users.noreply.github.com>
2024-11-14 16:27:14 -0500

    Added fetch request on course list for testing

--- alternate-career-planner/src/components/CourseList/CourseList.js
+++ alternate-career-planner/src/components/CourseList/CourseList.js
@@ -3,11 +3,28 @@
     and their hours.
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CourseList.css';
 
 // Lists courses, their description, and their number of credit hours
 const CourseListView = ({ courses }) => {
+    const [message, setMessage] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:5000/ryandata');
+                if (!response.ok) throw new Error('Network response was not ok');
+
+                const data = await response.json();
+                setMessage(data.message);
+            } catch (error) {
+                console.error("Fetch error:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     // Don't show anything (including title) if there are no courses
     if (!courses || courses.length === 0) {
@@ -26,8 +43,9 @@ const CourseListView = ({ courses }) => {
                     </li>
                 ))}
             </ul>
+            {message && <p>{message}</p>}
         </div>
-    )
-}
+    );
+};
 
 export default CourseListView;
