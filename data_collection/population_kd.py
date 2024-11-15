b99342ec8cc3f7011e7720c967593e7c3b819ff2
KaleDod <lsd728@vols.utk.edu>
2024-11-14 21:35:56 -0500

    updated print statements

--- data_collection/population_kd.py
+++ data_collection/population_kd.py
@@ -55,6 +55,12 @@ class Bucket:
     self.course_ids = set()
     self.other_bucket_ids = set()
 
+class Person:
+  def __init__(self, major, classes_array, quiz_results):
+    self.major = major
+    self.classes_array = classes_array
+    self.quiz_results = quiz_results
+
 
 # from the course table, build a course object with 
 # ID, description, name, credit hours, prereqs, and coreqs
@@ -71,12 +77,12 @@ def build_course_objects():
       #if this course ID already exists, add this current name to the list of names
       if course_id in course_objects.keys():
         # print(row, " key already made, adding ", course_table['Course'][row])
-        course_objects[course_id].names.add(course_table['Course'][row])
+        course_objects[course_id].names.add(course_table['Course'][row].replace(" ",""))
       else:
         #make a new course object for this entry
         # print(row," making new key, adding ", course_table.loc[row, 'Course ID'], " with names ",course_table['Course'][row] )
         course_object = Course(course_table.loc[row, 'Course ID'], course_table.loc[row, 'Course Description'], course_table.loc[row, 'Course Hours'])
-        course_object.names.add(course_table['Course'][row])
+        course_object.names.add(course_table['Course'][row].replace(" ",""))
         course_objects[str(course_id)] = course_object
 
   # PREREQs and COREQs
@@ -89,11 +95,11 @@ def build_course_objects():
   #for each row (each course and P/C pairing)
   for row in prereqs.index:
     #get this current row's course name and course ID
-    current_course_name = prereqs['Course'][row]
+    current_course_name = prereqs['Course'][row].replace(" ","")
     current_course_ID = prereqs['Course ID'][row]
 
     #get this current row's req class name or req course ID
-    current_req_name = prereqs['PreReq/CoReq Course'][row]
+    current_req_name = prereqs['PreReq/CoReq Course'][row].replace(" ","")
     current_req_ID = prereqs['Pre-Req Course ID'][row]
 
     #this whole row is empty
@@ -156,8 +162,8 @@ def build_bucket_objects(course_objects):
       for current_course_name in bucket_obj.course_names:
         # find this course ID in the course objects list
         for course_obj in course_objects.values():
-          # if(course_obj.name == "AE 377"): print("found")
           if(current_course_name in course_obj.names):
+            # print("found a matching name")
             bucket_obj.course_ids.add(course_obj.course_id)
 
       #add the sequence IDs (other bucket ids.. we can think of a better name)
@@ -228,6 +234,29 @@ def build_major_objects(course_objects, bucket_objects):
   
   return major_objects
 
+def adding_person(personCourseObjects, major, quiz_major, major_object):
+  # personCourseObjects is the array of course objects from Ryans parsing code 
+  # major will hopefully also be from ryan's code
+  # and then what they scored on the quiz is the quiz_major string 
+  # from the output from ryan's code , we will do the following 
+  # Ryan's code will create an array of course obects 
+
+  # major object is from our own stuff 
+
+  # changing both of the names into IDs so we can store those ids in our one person 
+  for majors in major_object:
+    if(major == majors.name):
+      major_id = majors.major_id 
+    if(quiz_major == majors.name):
+      quiz_id = majors.major_id
+  func_classes_array = []
+  for i in personCourseObjects:
+    if(personCourseObjects[i].grade > 'C'):
+      # if they got higher than a C, add it to the person object
+      func_classes_array.append(personCourseObjects[i].course_name)
+  OnlyOnePerson = Person(major=major_id, classes_array=func_classes_array, quiz_results=quiz_id)
+  return OnlyOnePerson
+
 def processing_course(course_objects, person_object):
   # list of strings
   # from ryan - list of strings
@@ -237,7 +266,8 @@ def processing_course(course_objects, person_object):
   for history_name in person_object.classes_array:
     for course in course_objects.values():
       # TODO if history name in course.names
-      if(course.name == history_name):
+      if(history_name in course.names):
+      # if(course.name == history_name):  
         history_ids.add(course.course_id)
   return history_ids
 
@@ -266,12 +296,12 @@ def compare_academic_history(person_object, major_objects, course_objects, bucke
           hour_counter += course_objects["course_taken"].hours
         # look up the course id in the major dictionary at this i 
 
-
 def print_course_obj(course_object):
   course_name = course_object.names
   course_hrs = course_object.hours
   course_prereq_id = course_object.prereq_courseids
-  print_statement = "Courses: "
+  print_statement = "\nCourses: "
+  print_statement += course_object.course_id + " "
   for y in course_name:
     print_statement += y + " "
 
@@ -285,7 +315,8 @@ def print_course_obj(course_object):
     for x in course_prereq_id:
       print_statement += x
       
-  return print(print_statement)
+  print_statement += "\n"
+  return print_statement
 
 
 def print_bucket_obj(bucket_object):
@@ -294,37 +325,78 @@ def print_bucket_obj(bucket_object):
 
   courses = bucket_object.course_names
 
-  print_statement = "Bucket: " + bucket_name + " has these courses "
+  print_statement = "\nBucket: " + bucket_name + " has these courses "
   for x in courses:
     print_statement += x + " "
   if num_hrs == -1:
     print_statement += "\n"
   else:
     print_statement += "\nFor a total of " + str(num_hrs) + " hours"
-  return print(print_statement) 
+  print_statement += "\n"
+  return print_statement
 
-def print_major_obj(major_object, course_obj): 
-  #set this up to take in course_obj so we can find them based on course_id set that this has
+def print_major_obj(major_object, course_object, bucket_object): 
   maj_cour_ids = major_object.course_ids
   maj_buck_ids = major_object.bucket_ids
   name = major_object.name
   print_statement = name
   for x in maj_cour_ids:
-    #print_course_obj(course_object=course_obj[x])
-    print(x)
-  #print(name)
-  return print_statement
-
+    if x in course_object:
+      print_statement += print_course_obj(course_object=course_object[x])
+  for y in maj_buck_ids:
+    if y in bucket_object:
+      #in here is where we can add the hard coded exceptions for the odd buckets that we already found
+      print_statement += print_bucket_obj(bucket_object=bucket_object[y])
+  return print(print_statement)
 
 def main():
   course_objects = build_course_objects()
-  bucket_objects = build_bucket_objects(course_objects)
+  """
+  for course_obj in course_objects.values():
+    print("Course Names: ", course_obj.names)
+    print("Course ID: ", course_obj.course_id)
+    print("Prereq IDs: ", course_obj.prereq_courseids)
+    print("Coreq IDs: ", course_obj.coreq_courseids)
+  """
 
-  for i in course_objects:
-    print_course_obj(course_object=course_objects[i])
-  for i in bucket_objects:
-    print_bucket_obj(bucket_object=bucket_objects[i])
+  bucket_objects = build_bucket_objects(course_objects)
+  """
+  for bucket_obj in bucket_objects.values():
+    print("Name: ", bucket_obj.name)
+    print("Course Names: ", bucket_obj.course_names)
+    print("Course IDs: ", bucket_obj.course_ids)
+    if bucket_obj.other_bucket_ids != set():
+      print("Other Bucket IDs: ", bucket_obj.other_bucket_ids)
+  """
+
 
   major_objects = build_major_objects(course_objects=course_objects, bucket_objects=bucket_objects)
-  for i in major_objects:
-    print_major_obj(major_object=major_objects[i], course_obj=course_objects)
+  """
+  for major_obj in major_objects.values():
+    print("Name: ", major_obj.name)
+    print("Course IDs: ", major_obj.course_ids)
+    if major_obj.bucket_ids != set():
+      print("Bucket IDs: ", major_obj.bucket_ids)
+  """
+
+  #for i in course_objects:
+    #print_course_obj(course_object=course_objects[i])
+  #for i in bucket_objects:
+    #print_bucket_obj(bucket_object=bucket_objects[i])
+  #for i in major_objects:
+    #print_major_obj(major_object=major_objects[i], course_object=course_objects, bucket_object=bucket_objects)
+  #print(print_course_obj(course_object=course_objects["136"]))
+  #print_major_obj(major_object=major_objects["10"], course_object=course_objects, bucket_object=bucket_objects)
+
+  majorid = 1
+  classes_array = ["EF 151", "EF 230", "MATH 141"]
+  quiz_results = "ME"
+  person_object = Person(major=majorid, classes_array=classes_array, quiz_results=quiz_results)
+  major_objects = build_major_objects(course_objects=course_objects, bucket_objects=bucket_objects)
+  #print(major_objects)
+  compare_academic_history(person_object=person_object, major_objects=major_objects, course_objects=course_objects, bucket_objects=bucket_objects)
+  print_major_obj(major_object=major_objects["5"], course_object=course_objects, bucket_object=bucket_objects)
+
+
+#optional
+main()
